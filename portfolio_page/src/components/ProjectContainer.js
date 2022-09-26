10bfa3bfce7a9c3ad51c8eb750764ad761e761a6
ascodeasice <ab0988956087@gmail.com>
2022-09-26 12:28:45 +0800

    Finish my project part.

--- portfolio_page/src/components/ProjectContainer.js
+++ portfolio_page/src/components/ProjectContainer.js
@@ -1,6 +1,11 @@
 import Project from "./Project";
 import InstagramClone from '../assets/photo/instagramClone.png';
 import Waldo from '../assets/photo/waldo.png';
+import IKEA from '../assets/photo/ikea.png';
+import MemoryCard from '../assets/photo/memoryCard.png';
+import BattleShip from '../assets/photo/battleship.png';
+import Todo from '../assets/photo/todo.png';
+import { useEffect } from 'react';
 
 function ProjectContainer() {
   const projects = [
@@ -17,20 +22,72 @@ function ProjectContainer() {
       liveLink: 'https://where-is-waldo-and-more.web.app/',
       githubLink: 'https://github.com/ascodeasice/where-is-waldo',
       src: Waldo
+    },
+    {
+      name: 'Fake IKEA',
+      description: 'A e-commerce website that imitates IKEA shopping website, allowing user to buy things.\n\nMade with React and React Router.',
+      liveLink: 'https://ascodeasice.github.io/shopping-cart/store',
+      githubLink: 'https://github.com/ascodeasice/shopping-cart',
+      src: IKEA
+    },
+    {
+      name: 'Memory Card',
+      description: 'A memory card game where you must remember what you clicked, and click other cards after they shuffle.\n\n Made with React',
+      liveLink: 'https://ascodeasice.github.io/memory-card/',
+      githubLink: 'https://github.com/ascodeasice/memory-card',
+      src: MemoryCard
+    },
+    {
+      name: 'Battleship',
+      description: 'he classic game \'Battleship\', made with TDD in JavaScript.\nYou have to find where the opponent\'s ship is, and sunken them.\n\nMade with Jest, Webpack, ESLint and Babel.',
+      liveLink: 'https://github.com/ascodeasice/battleship',
+      githubLink: 'https://github.com/ascodeasice/battleship',
+      src: BattleShip
+    },
+    {
+      name: 'To-do list',
+      description: 'A to-do list app that imitages visual design of Todoist app , the user can add/edit projects and tasks.\n\nThe user can also login with google, and save their edits.\n\nMade with Webpack and Firebase.',
+      liveLink: 'https://todo-list-6452b.web.app/',
+      githubLink: 'https://github.com/ascodeasice/todo-list',
+      src: Todo
     }
-  ]
+  ];
+
+  function scrollTrigger(selector) {
+    let els = document.querySelectorAll(selector)
+    els = Array.from(els)
+    els.forEach(el => {
+      addObserver(el)
+    })
+  }
+  function addObserver(el) {
+    // We are creating a new IntersectionObserver instance
+    let observer = new IntersectionObserver((entries, observer) => { // This takes a callback function that receives two arguments: the elements list and the observer instance.
+      entries.forEach(entry => {
+        // `entry.isIntersecting` will be true if the element is visible
+        if (entry.isIntersecting) {
+          entry.target.classList.add('slideFromBottom')
+          // We are removing the observer from the element after adding the active class
+          observer.unobserve(entry.target)
+        }
+      })
+    })
+    // Adding the observer to the element
+    observer.observe(el)
+  }
+
+  useEffect(() => {
+    scrollTrigger('.project')
+  }, []);
 
   return (
     <>
       <h1 className='title paddingLeft'>My Projects</h1>
       <div className="projectContainer">
         {
-          projects.map((project) => <Project name={project.name} description={project.description} githubLink={project.githubLink} liveLink={project.liveLink} src={project.src} />)
+          projects.map((project) => <Project name={project.name} description={project.description}
+            githubLink={project.githubLink} liveLink={project.liveLink} src={project.src} />)
         }
-        <Project />
-        <Project />
-        <Project />
-        <Project />
       </div>
     </>
   )
