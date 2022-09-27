1a41c0ee4606c4c1b2959481ac7572d8c496b278
ascodeasice <ab0988956087@gmail.com>
2022-09-27 17:40:07 +0800

    Finish media query of tablet.

--- portfolio_page/src/index.js
+++ portfolio_page/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import './styles/animation.css';
+import './styles/tablet.css';
+import './styles/mobile.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
